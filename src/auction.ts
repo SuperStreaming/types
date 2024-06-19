f0f0e0111205e46a478690d52256a61eea2259d6
seaders <seaders69@gmail.com>
2024-06-19 16:54:43 +0100

    Auction types added

--- src/auction.ts
+++ src/auction.ts
@@ -4,3 +4,61 @@ export type PlaceBidProps = {
   picUrl: string
   username: string
 }
+
+export type BidData = {
+  email?: string
+  auction_id: number
+  amount: number
+  quantity: number
+  public: number
+  proxy_bid: number
+  uid?: string
+  username?: string
+  picUrl?: string
+}
+
+export type BidRule = {
+  bid_from: string
+  bid_to: string
+  rate: string
+  maximum_rate: string
+}
+
+export type Auction = {
+  id: number
+  shopify_product_id: number
+  product_handle: string
+  product_title: string
+  popcorn_bidding: number
+  max_popcorn_bid: number
+  extend_deadline_by: string
+  extend_deadline_within: string
+  start_date: string
+  id_auction_status: number
+  end_date: string
+  booking_amount: string
+  buy_now_price: string
+  display_auction_days: number
+  auction_status: string
+  reserve_price: string
+  start_price: string
+  proxy_bidding_on: number
+  auto_pay_enable: number
+  active: number
+  joining_fee_enable: number
+  date_add: string
+  date_upd: string
+  max_bid: string
+  total_bid: number
+  now: string
+}
+
+export type AuctionContainer = {
+  auction: Auction
+  bid_rule: any[]
+  cust_date: string
+  booking_product: any
+  buyUrl: string
+  imgUrl: string
+  bid?: BidData
+}
