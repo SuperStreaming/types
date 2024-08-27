3905b10b15b89c8f951cd8991d912d2098d66b36
seaders <seaders69@gmail.com>
2024-08-27 12:42:51 +0100

    answerTimes added to player def

--- src/player.ts
+++ src/player.ts
@@ -1,38 +1,40 @@
-import { Timestamp } from "firebase/firestore";
-import { type MoneyType } from "./money";
+import { Timestamp } from "firebase/firestore"
+import { type MoneyType } from "./money"
 
-export type Answers = Record<string, string>;
-export type RapidTimers = Record<string, Timestamp>;
+export type Answers = Record<string, string>
+export type RapidTimers = Record<string, Timestamp>
 
 export type Player = {
-  id: string;
-  playerId: string;
-  username: string;
-  pictureUrl: string;
-  currentStep: number;
-  answers: Answers;
-  rapid?: RapidTimers;
-  isOnHotStreak: boolean;
-  isCaught: boolean;
-  isChaser: boolean;
+  id: string
+  playerId: string
+  eventId: string
+  username: string
+  pictureUrl: string
+  currentStep: number
+  answers: Answers
+  answerTimes: { [key: string]: Timestamp }
+  rapid?: RapidTimers
+  isOnHotStreak: boolean
+  isCaught: boolean
+  isChaser: boolean
   ticket: {
-    purchasedAt: Timestamp;
-    type: "virtual" | "real";
+    purchasedAt: Timestamp
+    type: "virtual" | "real"
     price: {
-      currency: string;
-      amount: number;
-    };
-  };
+      currency: string
+      amount: number
+    }
+  }
   earnings: {
     cash: {
-      estimated?: MoneyType;
-      offer: MoneyType;
-      real: MoneyType;
-    };
+      estimated?: MoneyType
+      offer: MoneyType
+      real: MoneyType
+    }
     coins: {
-      estimated?: MoneyType;
-      offer: MoneyType;
-      real: MoneyType;
-    };
-  };
-};
+      estimated?: MoneyType
+      offer: MoneyType
+      real: MoneyType
+    }
+  }
+}
